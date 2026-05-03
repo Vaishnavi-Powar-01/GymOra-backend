f2da8bac12125a3c4cfcdf241d23d8116591a897
[Vaishnavi-Powar-01] <[vaishnavipowar11@gmail.com]>
2026-05-03 13:39:11 +0530

    final

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['https://gym-ora-frontend.vercel.app/'],
   credentials: true,
 }));
 
